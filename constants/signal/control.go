91361e531f21021827fb75caa7725a6da359c333
ns-cn <ns-cn@qq.com>
2023-01-12 16:07:41 +0800

    iota

--- constants/signal/control.go
+++ constants/signal/control.go
@@ -3,8 +3,8 @@ package signal
 type Control int
 
 const (
-	QUIT     Control = 1
-	PAUSE    Control = 1 << 1
-	CONTINUE Control = 1 << 2
-	RESUME   Control = 1 << 3
+	QUIT Control = 1 << iota
+	PAUSE
+	CONTINUE
+	RESUME
 )
